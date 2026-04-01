9e9d2552532e9cffd53538a08fdda8bd2bc76bc0
hwb <hwb@dev.com>
2026-04-01 16:47:00 +0800

    添加初始loading动效；更新icon图片为192x192；

--- index.59ab9.js
+++ index.59ab9.js
@@ -1,4 +1,4 @@
-System.register(["./application.e2bce.js"], function (_export, _context) {
+System.register(["./application.01da7.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
